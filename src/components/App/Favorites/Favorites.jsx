7ea4e6ab2e555d03d4011bf25f14dc9839d10926
Zlo0oy <sergey.vasiliev.ua@gmail.com>
2022-02-11 19:59:44 +0200

    minor style fixes

--- src/components/App/Favorites/Favorites.jsx
+++ src/components/App/Favorites/Favorites.jsx
@@ -8,23 +8,30 @@ const Favorites = () => {
 
     const [movies, setMovies] = useState([]);
 
+    const getFavoriteMoviesFromLocalStorage = () => {
+        const movies = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
+        setMovies(movies);
+    }
+
     useEffect(
         () => {
-            const movies = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
-            console.log(movies)
-            setMovies(movies)
+            getFavoriteMoviesFromLocalStorage();
         },
-        [JSON.parse(localStorage.getItem('favoriteMovies'))]
+        []
     )
 
     return (
         <div className='content'>
             <div className='page-title'>
                 <p>My Favorite</p>
+                {!movies.length && (
+                    <p className='favorites-default-text'>No favorites yet</p>
+                )}
                 {movies.map(movie => (
                     <Movie 
                         key={movie.id} 
                         movie={movie}
+                        getMovies={getFavoriteMoviesFromLocalStorage}
                     />
                 ))}
             </div>
--- src/components/App/Favorites/Movie.js
+++ src/components/App/Favorites/Movie.js
@@ -1,36 +1,46 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 import './Movie.css';
 
-export const Movie = ({ movie }) => {
+export const Movie = ({ movie, getMovies }) => {
 
-    const removeFromFavorites = () => {
+    const navigate = useNavigate();
+
+    const removeFromFavorites = (event) => {
+        event.stopPropagation ();
         const movies = JSON.parse(localStorage.getItem('favoriteMovies'));
         const filtered = movies.filter(item => item.id !== movie.id);
         localStorage.setItem('favoriteMovies', JSON.stringify(filtered));
+        getMovies();
     }
 
-  return (
-    <div className='movie-container'>
-        <img 
-            src={process.env.REACT_APP_IMAGE_URL + movie.poster_path} 
-            alt={movie.title}
-            className='favorite-poster'
-        />
-        <div className='details'>
-            <div className='remove-from-favorites' 
+    const handleMovieClick = () => {
+        navigate(`/movie-details/${movie.id}`, { replace: true });
+    }
+
+    return (
+        <div className='movie-container' onClick={handleMovieClick}>
+            <img 
+                src={process.env.REACT_APP_IMAGE_URL + movie.poster_path} 
+                alt={movie.title}
+                className='favorite-poster'
+            />
+            <div className='details'>
+                <p className='movie-title'>
+                    {movie.title}
+                </p>
+                <p className='movie-overview'>
+                    {movie.overview}
+                </p>
+            </div>
+            <div
+                className='remove-from-favorites' 
                 onClick={removeFromFavorites}
             >
                 Unfavorite
             </div>
-            <p className='movie-title'>
-                {movie.title}
-            </p>
-            <p className='movie-overview'>
-                {movie.overview}
-            </p>
         </div>
-    </div>
   )
 }
